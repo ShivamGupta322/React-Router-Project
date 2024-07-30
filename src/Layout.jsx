bcb8bbff12e250470c464278ce1426e23b0cf51f
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-07-30 16:55:38 +0530

    added github api

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import User from './components/User/User'
 
 function layout() {
   return (
     <>
     <Header />
+    {/* <User /> */}
     <Outlet />
     <Footer />
     </>
